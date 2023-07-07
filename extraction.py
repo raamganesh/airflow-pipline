7a233ee34143ef6f715b007026f4a8fc1bcbd9d8
Raam Ganesh <m.p.raamganesh@gmail.com>
2023-07-07 15:18:34 +0100

    intial extraction of data from youtube and uploading to csv using airflow

---
+++ extraction.py
@@ -0,0 +1,131 @@
+import json
+import requests
+import config
+import pandas as pd
+import datetime
+import boto3
+import os
+
+
+import logging
+
+# Set up logging configuration
+# Create a directory in the script's location
+log_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "log")
+os.makedirs(log_directory, exist_ok=True)
+
+log_file_path = os.path.join(log_directory, f"logfile_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log")
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler(filename=log_file_path),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger()
+
+# Function to make API request
+def make_api_request(url, params):
+    try:
+        response = requests.get(url, params=params, timeout=10)
+        response.raise_for_status()
+        return json.loads(response.content)["items"]
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error making API request: {e}")
+        return None
+    except (json.JSONDecodeError, KeyError) as e:
+        logger.error(f"Error parsing API response: {e}")
+        return None
+
+def get_trending_videos():
+    """Gets the list of trending videos in the UK."""
+
+    api_key = config.API_KEY
+    regions = [ "IN", "GB", "US" ]
+    videos = []
+
+    for region in regions:
+        url = "https://www.googleapis.com/youtube/v3/videos"
+        params = {
+            "part": "snippet, statistics",
+            "regionCode": region,
+            "type": "video",
+            "chart": "mostPopular",
+            "maxResults": 20,
+            "key": api_key
+        }
+
+        data = make_api_request(url, params)
+        if data is not None:
+            data = [{**video, "region": region, "rank": rank + 1} for rank, video in enumerate(data)]
+            videos.extend(data)
+
+    return videos
+
+def tranform_data( videos ):
+    video_list = []
+    for video in videos:
+        video_dict = dict()
+        video_dict['region'] = video.get('region')
+        video_dict['video_ID'] = video.get('id')
+        video_dict['category_ID'] = video['snippet'].get('categoryId')
+        video_dict['channel_Title'] = video['snippet'].get('channelTitle')
+        video_dict['title'] = video['snippet']['localized'].get('title')
+        video_dict['published_date'] = datetime.datetime.strptime(video['snippet'].get('publishedAt'), "%Y-%m-%dT%H:%M:%SZ") if video['snippet'].get('publishedAt') else None
+        video_dict['rank'] = video.get('rank')
+        video_dict['tags'] = video['snippet'].get('tags')
+        video_dict['number_of_comments'] = video['statistics'].get('commentCount')
+        video_dict['like_Count'] = video['statistics'].get('likeCount')
+        video_dict['view_Count'] = video['statistics'].get('viewCount')
+    
+        video_list.append(video_dict)
+
+    today = datetime.date.today()
+    video_df = pd.DataFrame( video_list )
+
+    video_df.insert(0, 'Date', today)
+
+    return video_df
+
+
+def upload_dataframe_to_s3(dataframe, bucket_name, key):
+    csv_buffer = dataframe.to_csv(index=False)  # Convert DataFrame to CSV string
+
+    session = boto3.Session(
+        region_name='us-west-2',
+        aws_access_key_id=config.aws_access_key,
+        aws_secret_access_key=config.aws_secret_key
+    )
+
+    s3 = session.client('s3')
+
+    # Upload CSV data directly to S3 bucket
+    response = s3.put_object(
+        Body=csv_buffer.encode('utf-8'),  # Encode CSV string as bytes
+        Bucket=bucket_name,
+        Key=key
+    )
+
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        logger.info(f"File '{key}' uploaded successfully to S3 bucket '{bucket_name}'")
+    else:
+        logger.error(f"Failed to upload file '{key}' to S3 bucket '{bucket_name}'")
+
+def load_data( df ):
+    bucket_name = "youtube-etl-airflow"
+    filename = f"youtube_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
+
+    upload_dataframe_to_s3(df, bucket_name, filename)
+    
+
+
+videos = get_trending_videos()
+video_df = tranform_data( videos )
+
+load_data(video_df)
+
+
+
---
+++ youtube_etl_dag.py
@@ -0,0 +1,58 @@
+import json
+import pendulum
+import sys
+
+from datetime import datetime, timedelta
+
+from airflow.decorators import dag, task
+
+# To import code from different directory
+import extraction
+
+@dag(   
+        default_args={
+        "depends_on_past": False,
+        "email": ["m.p.raamganesh@gmail.com"],
+        "email_on_failure": True,
+        "email_on_retry": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
+        # 'queue': 'bash_queue',
+        # 'pool': 'backfill',
+        # 'priority_weight': 10,
+        # 'end_date': datetime(2016, 1, 1),
+        # 'wait_for_downstream': False,
+        # 'sla': timedelta(hours=2),
+        # 'execution_timeout': timedelta(seconds=300),
+        # 'on_failure_callback': some_function, # or list of functions
+        # 'on_success_callback': some_other_function, # or list of functions
+        # 'on_retry_callback': another_function, # or list of functions
+        # 'sla_miss_callback': yet_another_function, # or list of functions
+        # 'trigger_rule': 'all_success'
+    },
+        schedule="0 22 * * *",
+        start_date=pendulum.datetime(2023,7,7, tz='UTC'),
+        catchup=False,
+        tags=["youtube", "test"]
+)
+
+def youtube_etl():
+    @task
+    def extract_data_from_youtube():
+        videos = extraction.get_trending_videos()
+        return videos
+    
+    @task
+    def tranform_data( videos ):
+        data_frame = extraction.tranform_data( videos )
+        return data_frame
+    
+    @task
+    def upload_csv_to_s3( data_frame ):
+        extraction.load_data( data_frame )
+
+    videos = extract_data_from_youtube()
+    data_frame = tranform_data( videos )
+    upload_csv_to_s3( data_frame )
+
+youtube_etl()
